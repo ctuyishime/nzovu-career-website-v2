80bb841a20ba5d82dc7ac585a59b795538d23983
ctuyishime <tuyishimecharles@gmail.com>
2023-12-22 21:53:46 -0600

    added secrets

--- database.py
+++ database.py
@@ -3,12 +3,13 @@ from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_USERNAME = os.getenv("DB_USERNAME")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_NAME = os.getenv("DB_NAME")
 
 db_connection_string = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4" 
+
 engine = create_engine(db_connection_string, connect_args={
         "ssl": {
             "ca": "/etc/ssl/cert.pem"
