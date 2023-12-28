637a6c50be1ba8b80d55f9ea5c0b7e41cd003fe2
ctuyishime <tuyishimecharles@gmail.com>
2023-12-28 01:21:48 -0600

    created a job page

--- database.py
+++ database.py
@@ -20,7 +20,30 @@ engine = create_engine(db_connection_string, connect_args={
 def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs"))
-        
         columns = result.keys()
-        jobs = [{column: value for column, value in zip(columns, row)} for row in result]
-    return jobs
+        jobs = []
+        for row in result:
+            jobs_dict = {}
+            for column, value in zip(columns, row):
+                jobs_dict[column] = value
+            jobs.append(jobs_dict)
+        return jobs
+
+def load_job_from_db(job_id):
+    with engine.connect() as conn:
+        # Execute the query with the provided job ID
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"), {'val': job_id}
+        )
+        # Retrieve the column names from the result
+        columns = result.keys()
+        # Initialize a variable to hold the job data
+        job_dict = None
+        for row in result:
+            job_dict = {}
+            for index, column in enumerate(columns):
+                job_dict[column] = row[index]
+            break
+        return job_dict
+
+
